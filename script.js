3c2fbf6c9abb186e3e7d669cd904ae377b4937c2
asin0002 <asin0002@algonquinlive.com>
2026-02-09 15:30:51 -0500

    Update comments for  student details

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// 1. Select the gallery element from the DOM
+// name: atungdiwe asinah  student number:04110443
+
+//  Select the gallery element from the DOM
 const $gallery =document.getElementById('emoji-gallery');
 
 //  array of emoji codes
@@ -7,12 +9,12 @@ const emojiCodes = [
   128518, 128519, 128520, 128521, 128522, 128523
 ];
 
-// 3. Create an empty array to store the HTML for each emoji card
+//  Create an empty array to store the HTML for each emoji card
 const emojiCards = [];
 
-// 4. Loop over the array to build the HTML for each emoji
+//  Loop over the array to build the HTML for each emoji
 for (const code of emojiCodes) {
-  // Requirement: Include decimal reference next to character
+  
   emojiCards.push(`
     <div class="card">
       <span class="emoji">&#${code};</span>
@@ -21,5 +23,5 @@ for (const code of emojiCodes) {
   `);
 }
 
-// 5. Add all emojis to the grid at once for better performance
+//  Add all emojis to the grid at once for better performance
 $gallery.innerHTML = emojiCards.join('');
